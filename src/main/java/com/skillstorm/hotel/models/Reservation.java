08109ef4977536653b8d37b20c03bef35dc06e25
Leo-Kopec <lfkopec@loyola,edu>
2022-07-27 15:41:35 -0400

    More Hiberate Annontations for Reservation Model

--- src/main/java/com/skillstorm/hotel/models/Reservation.java
+++ src/main/java/com/skillstorm/hotel/models/Reservation.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
@@ -22,12 +24,14 @@ public class Reservation {
 	
 	@NotNull
 	@Column(name = "reserv_guest")
-	// Map to Guest Table
+	@ManyToOne
+	@JoinColumn(name = "guest_id")
 	private Guest guest;
 	
 	@NotNull
 	@Column(name = "reserv_room")
-	// Map to Room Table
+	@ManyToOne
+	@JoinColumn(name = "room_id")
 	private Room room;
 	
 	@Future
