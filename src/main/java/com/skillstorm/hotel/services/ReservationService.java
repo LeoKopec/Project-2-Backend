c6cab6c155340efafb50b321ef4002558c69f67c
Leo-Kopec <lfkopec@loyola,edu>
2022-07-28 16:57:13 -0400

    Implemented Put, Post, and Delete Methods for Reservations

--- src/main/java/com/skillstorm/hotel/services/ReservationService.java
+++ src/main/java/com/skillstorm/hotel/services/ReservationService.java
@@ -1,5 +1,14 @@
 package com.skillstorm.hotel.services;
 
+import java.util.List;
+
+import com.skillstorm.hotel.models.Reservation;
+
 public interface ReservationService {
 
+//	List<ReservationDTO> findByEmail();
+	Reservation save(Reservation reservation);
+	Reservation update(Reservation reservation);
+	void deleteById(int id);
+	
 }
--- src/main/java/com/skillstorm/hotel/services/ReservationServiceImpl.java
+++ src/main/java/com/skillstorm/hotel/services/ReservationServiceImpl.java
@@ -1,5 +1,34 @@
 package com.skillstorm.hotel.services;
 
-public class ReservationServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.skillstorm.hotel.models.Reservation;
+import com.skillstorm.hotel.repositories.ReservationRepository;
+
+@Service
+@Primary
+@Transactional
+public class ReservationServiceImpl implements ReservationService{
+
+	@Autowired
+	private ReservationRepository repository;
+	
+	@Override
+	public Reservation save(Reservation reservation) {
+		return repository.save(reservation);
+	}
+
+	@Override
+	public Reservation update(Reservation reservation) {
+		return repository.save(reservation);
+	}
+
+	@Override
+	public void deleteById(int id) {
+		repository.deleteById(id);
+	}
 
 }
