2d4a4b6825fa781ff142aeb044a20fb967f35f71
kmc369 <keishacoleman710@gmail.com>
2022-07-28 21:11:32 -0400

    GuestDone

--- src/main/java/com/skillstorm/hotel/service/GuestService.java
+++ src/main/java/com/skillstorm/hotel/service/GuestService.java
@@ -1,6 +1,6 @@
 package com.skillstorm.hotel.service;
 
-import java.util.List;
+
 
 import com.skillstorm.hotel.models.Guest;
 
--- src/main/java/com/skillstorm/hotel/service/GuestServiceImpl.java
+++ src/main/java/com/skillstorm/hotel/service/GuestServiceImpl.java
@@ -1,17 +1,14 @@
 package com.skillstorm.hotel.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.skillstorm.hotel.Repository.GuestRepository;
 import com.skillstorm.hotel.models.Guest;
 
-
 @Service
+@Primary
 public class GuestServiceImpl implements GuestService {
 
 	@Autowired
@@ -21,9 +18,5 @@ public class GuestServiceImpl implements GuestService {
 	public Guest save(Guest guest) {
 		return repo.save(guest);
 	}
-
-	
-
-	
 
 }
