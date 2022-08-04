b4fc9af86f08e7d36a3b226ddebc05df45900095
Derek Enlow <theunnamed97@gmail.com>
2022-08-04 14:59:07 -0600

    added room by id and date range endpoint

--- src/main/java/com/skillstorm/hotel/controllers/RoomController.java
+++ src/main/java/com/skillstorm/hotel/controllers/RoomController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skillstorm.hotel.dtos.RoomDTO;
 import com.skillstorm.hotel.models.Room;
+import com.skillstorm.hotel.services.RoomDTOMapper;
 import com.skillstorm.hotel.services.RoomService;
 
 @RestController
@@ -30,6 +32,19 @@ public class RoomController {
 
 	@Autowired
 	private RoomService service;
+	@Autowired
+	private RoomDTOMapper mapper;
+	
+//	/rooms/{id}?start=DATE&end=DATE
+	@GetMapping("/{id}")
+	public RoomDTO getById(
+			@PathVariable(name = "id") int roomId,
+			@RequestParam(name = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+			@RequestParam(name = "end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+			) {
+		Room room = service.findById(roomId);
+		return this.mapper.toDto(room, startDate, endDate);
+	}
 	
 //	/rooms/{id}/pricing?start=DATE&end=DATE
 	@GetMapping("/{id}/pricing")
@@ -46,6 +61,5 @@ public class RoomController {
 		.reduce((p1, p2) -> {return p1 + p2;});
 		return optD.isPresent() ? optD.getAsDouble() : 0;
 	}
-	
 	
 }
